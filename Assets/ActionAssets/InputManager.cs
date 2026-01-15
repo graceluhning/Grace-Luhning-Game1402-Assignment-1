93098ad2f203a50caa13b2d69ba6b34fc5e8c34d
Grace Luhning <graceluhning@gmail.com>
2026-01-14 18:27:28 -0700

    Week2
    Added Jump

---
+++ Assets/ActionAssets/InputManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerInputActions _playerInputActions;
+
+    public System.Action OnJump;
+    public System.Action<float> OnMove;
+
+    void Awake()
+    {
+        _playerInputActions = new PlayerInputActions(); // created an object
+        _playerInputActions.Enable();
+    }
+
+    void OnEnable()
+    {
+        _playerInputActions.Player.Jump.performed += OnJumpPressed;
+       // _playerInputActions.Player.Horizontal.performed += OnMovement;
+    }
+
+    void OnDisable()
+    {
+        _playerInputActions.Player.Jump.performed -= OnJumpPressed;
+        //_playerInputActions.Player.Horizontal.performed -= OnMovement;
+    }
+
+    void OnJumpPressed(InputAction.CallbackContext ctx)
+    {
+        OnJump?.Invoke();
+    }
+
+    void OnMovement()
+    {
+        //Debug.Log(ctx.ReadValue<float>());
+        OnMove?.Invoke(_playerInputActions.Player.Horizontal.ReadValue<float>());
+    }
+
+    void Update()
+    {
+        OnMovement();
+    }
+
+}
+      
